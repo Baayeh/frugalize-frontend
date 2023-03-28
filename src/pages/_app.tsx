e4db05bd4eca4ca2b6dd1634406dcd6b8281644e
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-03-28 17:05:39 +0000

    Add and style splash screen component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import type { AppProps } from 'next/app';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#fff',
+      contrastText: '#222',
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
