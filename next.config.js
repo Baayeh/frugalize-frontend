64a53cd5a964fdb4dcfec9292b86af34b29c9978
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-03-28 06:55:02 +0000

    Initial Setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname:
+          '/dskl0qde4/image/upload/v1679985230/frugalize_logo-blue_luxoa0.png',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
