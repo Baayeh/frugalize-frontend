c83cb0c11edee9f6c29cb2b6f4dc0780d334dc92
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-04 06:46:16 +0000

    Create a global file to hold all type declarations

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -22,3 +22,21 @@ export type PasswordVisibilityProps = {
   handleClickShowPassword: () => void;
   handleMouseDownPassword: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
+
+export type AddCategoryProps = {
+  cat_name: string;
+  image: string | null;
+};
+
+export type AddTransactionProps = {
+  transaction_name: string;
+  amount: string;
+  category_id: string;
+};
+
+export type NewUserProps = {
+  name: string;
+  username: string;
+  income: number;
+  email: string;
+}
