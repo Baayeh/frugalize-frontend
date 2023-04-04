7fd4eb42e0191b380e622d2265f13d0f5185bff8
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-04 06:43:57 +0000

    Add component to list all transactions

---
+++ src/pages/transactions/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const AllTransactions = (props: Props) => {
+  return (
+    <div>
+      <h1>AllTransactions</h1>
+    </div>
+  );
+};
+
+export default AllTransactions;
