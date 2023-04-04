b8e95a0fe0776242208d16e9a5dd310b9f196c21
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-04 06:47:08 +0000

    Provide the store to the components

---
+++ src/redux/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import userSlice from './users/userSlice';
+
+const store = configureStore({
+  reducer: {
+    user: userSlice,
+  },
+});
+
+export default store;
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
