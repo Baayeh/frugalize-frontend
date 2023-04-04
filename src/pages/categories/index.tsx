5637436c647f4dd266dfab694430557bf8c6239d
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-04 06:42:43 +0000

    Add component to list all categories

---
+++ src/pages/categories/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type Props = {};
+
+const AllCategories = (props: Props) => {
+  return (
+    <div>
+      <h1>AllCategories</h1>
+    </div>
+  );
+};
+
+export default AllCategories;
