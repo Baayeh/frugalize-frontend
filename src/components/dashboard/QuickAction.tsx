e41467a4081918731f0638adab7356a0a00a0fc0
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-04 06:34:04 +0000

    Create a quick action component

---
+++ src/components/dashboard/QuickAction.tsx
@@ -0,0 +1,54 @@
+import styles from '@/styles/Dashboard.module.css';
+import Link from 'next/link';
+import React from 'react';
+import { GiPayMoney } from 'react-icons/gi';
+import { MdAccountBalance, MdCategory, MdHistory } from 'react-icons/md';
+
+type Props = {};
+
+const QuickAction = (props: Props) => {
+  return (
+    <section className="mt-5">
+      <div className="relative mini-bar p-2 rounded-2xl flex justify-around bg-[#97fce6] text-black">
+        <Link
+          href="/dashboard/history"
+          className={`${styles.action} flex flex-col justify-center items-center`}
+        >
+          <span>
+            <MdHistory className="text-xl" />
+          </span>
+          <span className="text-sm">History</span>
+        </Link>
+        <Link
+          href="/dashboard/account"
+          className={`${styles.action} flex flex-col justify-center items-center`}
+        >
+          <span>
+            <MdAccountBalance className="text-xl" />
+          </span>
+          <span className="text-sm">Account</span>
+        </Link>
+        <Link
+          href="/dashboard/categories"
+          className={`${styles.action} flex flex-col justify-center items-center`}
+        >
+          <span>
+            <MdCategory className="text-xl" />
+          </span>
+          <span className="text-sm">Categories</span>
+        </Link>
+        <Link
+          href="/dashboard/transactions"
+          className={`${styles.action} flex flex-col justify-center items-center`}
+        >
+          <span>
+            <GiPayMoney className="text-xl" />
+          </span>
+          <span className="text-sm">Transactions</span>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default QuickAction;
