10c1f021ecca4dc3ab27b8bb17ea7cafd98f6263
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-04-04 06:40:27 +0000

    Modify MUI theme and include it in all components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,43 @@
+import { Providers } from '@/components/Provider';
 import '@/styles/globals.css';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import createEmotionCache from '@/utils/createEmotionCache';
+import { theme } from '@/utils/theme';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#fff',
-      contrastText: '#222',
-    },
-  },
-});
+interface AppCustomProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 
-export default function App({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: AppCustomProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Frugalize</title>
+        <meta
+          name="description"
+          content="Manage your expenses in a frugal way"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Providers>
+            <Component {...pageProps} />
+          </Providers>
+        </ThemeProvider>
+      </CacheProvider>
+    </>
   );
 }
